748f785419efb157f62ac478f1f8070e100e9d81
Elton Dornelas <eltondornelas@gmail.com>
2019-09-13 13:09:59 -0300

    About view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -30,13 +39,41 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
 	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		
 		
+	}
+	
+	//o sincronado é para garantir que a função não será interrompida. Funções de interface são multi thread.
+	private synchronized void loadView(String absoluteName) {
+		//função para abrir outra tela
+		//absoluteName porque vai passar o caminho completo gui/about.fxml
+				
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName)); //objeto que carrega a tela
+			VBox newVBox = loader.load();
+			
+			//mostrar essa view dentro da janela principal, precisa pegar uma referência da cena (scene) que foi declarada no Main.java (mainScene)
+			Scene mainScene = Main.getMainScene();
+			
+			//os filhos (children) da tela about tem que ficar dentro dos filhos da tela principal (MainView)
+			//precisa pegar a referência do vbox da tela principal
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent(); //pega o primeiro elemento da view que é o ScrollPane. precisa fazer o UpCast
+			
+			Node mainMenu = mainVBox.getChildren().get(0); //primeiro filho do VBox da janela principal = mainMenu
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+			//mostra o alerta na tela			
+		}
 	}
 
 	
